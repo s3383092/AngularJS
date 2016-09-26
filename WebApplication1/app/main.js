fc124d08522cc9bd2c2111d8592c5a8aca80194b
s3383092 <s3383092@student.rmit.edu.au>
2016-09-26 21:45:46 +1000

    Initial commit

---
+++ WebApplication1/app/main.js
@@ -0,0 +1,29 @@
+﻿var app = angular.module('app',['ngRoute'])
+
+app.controller("people", function ($scope, $http) {
+    $http.get("database.json")
+    .success(function (response) {
+        $scope.persons = response.records;
+    });
+});
+
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when('/table', {
+        templateUrl: "templates/table.html"
+    })
+    .when('/register', {
+        templateUrl: "templates/register.html"
+    })
+    .when('/home', {
+        templateUrl: "templates/mainPage.html"
+    })
+    .otherwise({
+        templateUrl: 'templates/mainPage.html'
+    });
+});
+
+$scope.isActive = function (viewLocation) {
+    var active = (viewLocation === $location.path());
+    return active;
+};
